bbf840a5bf986f1dbadd238ab8f9c24e0eab53cd
Kent Fredric <kentfredric@gmail.com>
2017-09-25 01:29:49 +1300

    Add ninja URL import script

---
+++ tools/urlimport.pl
@@ -0,0 +1,112 @@
+#!perl
+use strict;
+use warnings;
+use feature 'say';
+
+my $url = $ARGV[0] || die "Give URL to fetch";
+
+use HTTP::Tiny; 
+use Path::Tiny qw( path );
+use CPAN::DistnameInfo;
+
+my $wd; 
+BEGIN {
+  $wd = path('/tmp/urlimport_dir');
+  $wd->mkpath;
+}
+
+my $ua; 
+BEGIN {
+  $ua = HTTP::Tiny->new();
+}
+
+my ($tname,) = ( $url =~ qr{/([^/]+)\z} );
+
+say "$url => $tname";
+
+my $response = $ua->mirror( $url, $wd->child($tname) );
+if ( not $response->{success} ) {
+  die "Could not fetch $url: $response->{status} $response->{reason}";
+}
+
+$ENV{TZ} = "UTC";
+
+## Get timestap
+{
+  open my $fh, '-|', 'tar', '-vtf', $wd->child($tname)->stringify or die "Can't untar $tname";
+  my %seen_timestamps;
+  while ( my $line = <$fh> ) {
+    chomp $line;
+    $line =~ s/\A\S+\s\S+\s//;
+    $line =~ s/\A\s*\d+\s//;
+    my ( $timestamp, ) = ($line =~ qr{\A(\S+\s*\S+)});
+    $seen_timestamps{$timestamp}++;
+  }
+  my ( $oldest, ) = reverse sort keys %seen_timestamps;
+  say "Timestamp: $oldest";
+  $ENV{GIT_COMMITTER_DATE} = $oldest;
+  $ENV{GIT_AUTHOR_DATE} = $oldest;
+}
+if ( path('.gitignore')->exists ) {
+  system('git', 'rm', '.gitignore');
+}
+# Nuke existing files
+{
+  open my $fh, '-|', 'git', 'ls-files','-z' or die "Can't spawn ls-files";
+  local $/ = qq{\0};
+  my ( @buf );
+  while ( my $filename = <$fh> ) {
+    chomp $filename;
+    push @buf, $filename;
+    say "\e[31m$filename\e[0m";
+    if (( 1000 < length join q[ ], @buf )  or @buf > 1000 ) {
+      system('git','rm', '-f', @buf ) == 0 or die "Can't delete!";
+      @buf = ();
+    }
+  }
+  if( @buf ) {
+      system('git','rm', '-f', @buf ) == 0 or die "Can't delete!";
+      @buf = ();
+  }
+}
+# Unroll new tar
+{
+  system('tar','--strip-components=1', '-xvf', $wd->child($tname)->stringify ) == 0 or die "Tar bailed";
+}
+# Add new files
+{
+  system('git', 'add', '.' ) == 0 or die "Can't add!";
+}
+
+my $info = CPAN::DistnameInfo->new($url);
+
+my $tree;
+{
+  open my $fh, '-|', 'git', 'write-tree' or die "Write tree failed";
+  local $/ = undef;
+  $tree = scalar <$fh>;
+  $tree =~ s/\s*\z//m;
+  close $fh or die "Write tree failed";
+}
+say "tree SHA1 is $tree";
+my $commit;
+{
+  $ENV{GIT_AUTHOR_NAME} = $info->cpanid;
+  $ENV{GIT_AUTHOR_EMAIL} = lc($info->cpanid) . '@cpan.org';
+  $ENV{GIT_COMMITTER_NAME} = 'Kent Fredric';
+  $ENV{GIT_COMMITTER_EMAIL} = 'kentnl@cpan.org';
+
+  my $mesg = "Import $url";
+
+  my (@parents) = ( '-p','HEAD' );
+  if ( $ENV{EXTRA_PARENT} ) {
+    push @parents, '-p', $ENV{EXTRA_PARENT};
+  }
+  open my $fh, '-|', 'git','commit-tree', $tree, '-m', $mesg, @parents;
+  local $/ = undef;
+  $commit = scalar <$fh>;
+  $commit =~ s/\s*\z//m;
+  close $fh or die "Commit tree failed";
+}
+say "commit SHA1 is $commit";
+system("git","update-ref","-m", "Adding $url", "HEAD", $commit) == 0 or die "Can't update-ref";
