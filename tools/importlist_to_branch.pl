9b64ad3e2b400e3808277cb3414c6243872b3b96
Kent Fredric <kentnl@gentoo.org>
2017-10-21 12:42:58 +1300

    Add tool for converting URL/SHA pairs into grafted history

---
+++ tools/importlist_to_branch.pl
@@ -0,0 +1,36 @@
+#!perl
+use strict;
+use warnings;
+
+use Path::Tiny qw( path );
+
+use constant mydir => path($0)->absolute->parent;
+
+my @import_list = path( $ARGV[0] )->lines_raw( { chomp => 1 } );
+
+system( 'git', 'checkout', '-b', 'releases' );
+{
+    my $line = shift @import_list;
+    my ( $commit, @parents ) = split qr/\s+/, $line;
+    if ( not @parents ) {
+        local $ENV{NO_PARENTS} = 1;
+        system( $^X, path( mydir, 'urlimport.pl' ), $commit );
+    }
+    else {
+        local $ENV{NO_PARENTS} = 1;
+        local $ENV{EXTRA_PARENT} = join q[ ], @parents;
+        system( $^X, path( mydir, 'urlimport.pl' ), $commit );
+    }
+}
+while (@import_list) {
+    my $line = shift @import_list;
+    my ( $commit, @parents ) = split qr/\s+/, $line;
+    if ( not @parents ) {
+        system( $^X, path( mydir, 'urlimport.pl' ), $commit );
+    }
+    else {
+        local $ENV{EXTRA_PARENT} = join q[ ], @parents;
+        system( $^X, path( mydir, 'urlimport.pl' ), $commit );
+    }
+}
+
