1204cbf6bd09ec9feb99f73d4aedd42b6017b233
Kent Fredric <kentnl@gentoo.org>
2018-05-16 08:09:58 +1200

    Add step skipping support

--- tools/importlist_to_branch.pl
+++ tools/importlist_to_branch.pl
@@ -9,10 +9,19 @@ use constant mydir => path($0)->absolute->parent;
 my @import_list = path( $ARGV[0] )->lines_raw( { chomp => 1 } );
 
 my $steps = 0;
+my $steps_skipped = 0;
+
 
 system( 'git', 'checkout', '-b', 'releases' );
 {
     my $line = shift @import_list;
+    if ( $ENV{STEP_SKIP} and $ENV{STEP_SKIP} > 0 ) {
+      while( $steps_skipped < $ENV{STEP_SKIP} ) {
+        warn "\e[32mSkipped:\e[0m $line\n";
+        $line = shift @import_list;
+        $steps_skipped++;
+      }
+    }
     my ( $commit, @parents ) = split qr/\s+/, $line;
     if ( $commit =~ /\A\d\d\d\d-\d\d-\d\dT/ ) {
       $commit = shift @parents;
@@ -35,6 +44,7 @@ while (@import_list) {
     if ( $commit =~ /\A\d\d\d\d-\d\d-\d\dT/ ) {
       $commit = shift @parents;
     }
+    next if exists $ENV{STEP_SKIP} and $ENV{STEP_SKIP} <= $steps;
     if ( not @parents ) {
         system( $^X, path( mydir, 'urlimport.pl' ), $commit );
     }
